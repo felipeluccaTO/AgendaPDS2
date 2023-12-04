fefa49f53146ffde3a19631ce2c71b7782d0c4ee
Samuel Lima Horta <148572679+samucao1803@users.noreply.github.com>
2023-12-04 12:12:20 -0300

    Delete Codigo que roda directory

--- Codigo que roda/Excecoes.cpp
+++
@@ -1,9 +0,0 @@
-#include "Excecoes.hpp"
-
-#include <iostream>
-
-void handleExcecao(const std::exception& e) {
-    
-    /// Mensagem de erro a ser exibida em caso de excecao gerada
-    std::cerr << "Erro: " << e.what() << std::endl;
-}
